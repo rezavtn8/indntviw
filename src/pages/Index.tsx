9edd96475bfb39ecef76ba69cee4fa4efb58f483
Mohammadreza Vatankhah <mohammadrvatankhah@gmail.com>
2026-04-16 19:22:26 +0000

    Changes

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,7 +5,7 @@ const Index = () => {
   return (
     <>
       <Helmet>
-        <title>IndentView - Nanoindentation Data Visualization</title>
+        <title>Indentview — Nanoindentation Data Visualization</title>
         <meta name="description" content="Professional, science-grade visualization platform for nanoindentation data with interactive 2D heatmaps and 3D point clouds." />
       </Helmet>
       <IndentViewApp />
